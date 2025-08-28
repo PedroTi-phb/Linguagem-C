9db827ca566445cf5411925e8a88eaf282969f22
Pedro Henrique <pedromanda32@gmail.com>
2025-08-27 21:23:02 -0300

    Aqui compilo um código de switch case.
    Nesse código crio um mine menu de relação de letra informada pelo usuário.

--- main.c
+++ main.c
@@ -1,14 +1,31 @@
-//somar e tirar a media de dois valores
-#include<stdio.h>
-#include<stdlib.h>
-int main() {
-    double val1;
-    double val2;
-    printf("informe primeiro numero\n");
-    scanf("%lf", &val1);
-    printf("informe segundo numero\n");
-    scanf("%lf", &val2);
-    double media = (val1+val2)/2;
-    printf("o resultado da media dos valores: %.2lf\n ", media);
-    return 0;
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+ char conceito;
+ printf("informe de A a D do conceito \n");
+ scanf("%c", &conceito);
+ switch (conceito){
+     case 'A': {
+         printf(" A = Excelente");
+           break;}
+     case 'B':{
+        printf(" B =  Bom");
+          break;}
+          case 'C':{
+        printf(" C =  Regular");
+          break;}
+          case 'D':{
+        printf(" D =  Reprovado");
+          break;}
+
+          default : {
+           printf("nao informe letra certa");
+          }
+
+ }
+  return 0 ;
+
+
 }
