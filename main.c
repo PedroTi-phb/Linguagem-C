e5b5a0626ce72a96ca26aa232a73e3f7dca29dc0
Pedro Henrique <pedromanda32@gmail.com>
2025-09-10 13:41:04 -0300

    Imprimi maior,menor e media dos numeros
    Uso for para o usuario imprimir 10 vezes, depois condição de atualização de variavel, a media, e por fim o printf

--- main.c
+++ main.c
@@ -1,31 +1,37 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
- char conceito;
- printf("informe de A a D do conceito \n");
- scanf("%c", &conceito);
- switch (conceito){
-     case 'A': {
-         printf(" A = Excelente");
-           break;}
-     case 'B':{
-        printf(" B =  Bom");
-          break;}
-          case 'C':{
-        printf(" C =  Regular");
-          break;}
-          case 'D':{
-        printf(" D =  Reprovado");
-          break;}
+int main(void) {
+    int i, num, maior, menor, soma = 0;
+    float media;
 
-          default : {
-           printf("nao informe letra certa");
-          }
+    // Primeiro número (inicializa maior, menor e soma)
+    printf("Informe o primeiro numero: ");
+    scanf("%d", &num);
 
- }
-  return 0 ;
+    maior = num;
+    menor = num;
+    soma  = num;
 
+    // Lê os outros 9 números
+    for (i = 2; i <= 10; i++) {
+        printf("Informe um valor positivo e inteiro: ");
+        scanf("%d", &num);
 
+        soma += num;
+
+        if (num > maior) {
+            maior = num;
+        }
+        if (num < menor) {            // < para atualizar o menor
+            menor = num;
+        }
+    }
+
+    media = soma / 10.0f;             // divisão real
+
+    printf("\nO maior numero é %d\n", maior);
+    printf("O menor numero é %d\n", menor);
+    printf("A media dos numeros informados é %.2f\n", media);
+
+    return 0;
 }
