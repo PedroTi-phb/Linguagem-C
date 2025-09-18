9107498ea875817953d548df6b64d890d5bd944f
Pedro Henrique <pedromanda32@gmail.com>
2025-09-18 20:22:46 -0300

    Conta quantas notas para um valor total pedido!

--- main.c
+++ main.c
@@ -1,51 +1,32 @@
 #include <stdio.h>
 
 int main() {
-    int opcao;
-    int a, b;
-    int resultado;
+    int valor;
 
-    do {
-        printf("\n=== CALCULADORA ===\n");
-        printf("1 - Multiplicar\n");
-        printf("2 - Somar\n");
-        printf("3 - Subtrair\n");
-        printf("4 - Dividir\n");
-        printf("5 - Sair\n");
-        printf("Escolha uma opcao: ");
+    printf("Quanto voce deseja sacar ?\n");
+    scanf("%d", &valor);
 
-        scanf("%d", &opcao);           // <-- LÊ A OPÇÃO (fundamental!)
+    while (valor != 0) {
+        int n50, n10, n5, n1;
 
-        if (opcao >= 1 && opcao <= 4) {
-            printf("Digite o primeiro valor: ");
-            scanf("%d", &a);
-            printf("Digite o segundo valor: ");
-            scanf("%d", &b);
-        }
+        n50 = valor / 50;
+        valor = valor % 50;
 
-        if (opcao == 1) {
-            resultado = a * b;
-            printf("Resultado: %d\n", resultado);
-        } else if (opcao == 2) {
-            resultado = a + b;
-            printf("Resultado: %d\n", resultado);
-        } else if (opcao == 3) {
-            resultado = a - b;
-            printf("Resultado: %d\n", resultado);
-        } else if (opcao == 4) {
-            if (b == 0) {
-                printf("Erro: divisao por zero!\n");
-            } else {
-                resultado = a / b;       // divisao inteira
-                printf("Resultado: %d\n", resultado);
-            }
-        } else if (opcao == 5) {
-            printf("Encerrando programa\n");  // <-- agora com ';'
-        } else {
-            printf("Opcao invalida\n");
-        }
+        n10 = valor / 10;
+        valor = valor % 10;
 
-    } while (opcao != 5);               // para quando o usuario digitar 5
+        n5 = valor / 5;
+        valor = valor % 5;
 
+        n1 = valor / 1;
+        valor = valor % 1;
+
+        printf("Notas: B$50=%d B$10=%d B$5=%d B$1=%d\n", n50, n10, n5, n1);
+
+        printf("Digite outro valor (ou zero para sair): ");
+        scanf("%d", &valor);
+    }
+
+    printf("Programa encerrado\n");
     return 0;
 }
